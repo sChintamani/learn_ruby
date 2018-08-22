a94dc4cc0792931f094c14f5da49d9c82ce11c8f
Srinivas Chintamani <srinivas.chintamani@gmail.com>
2018-08-22 08:08:07 +0530

    add, subtract and sum are passing

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,16 @@
 #write your code here
+def add(x, y)
+  x + y
+end
+
+def subtract(subtrahend, minuend)
+  subtrahend - minuend
+end
+
+def sum(num_arr)
+  total = 0
+  num_arr.each do |num|
+    total += num
+  end
+  total
+end
