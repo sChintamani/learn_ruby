d51c4e493407b46f6e14ae18f534a99f2ebace56
Srinivas Chintamani <srinivas.chintamani@gmail.com>
2018-09-01 01:36:51 +0530

    implemented till qu, three more to go !

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,10 +1,23 @@
-def translate(inp, how_many_consonants_at_the_beginning = 1)
+def translate(inp)
+  outp = []
+  inp.split(" ").each do |word|
+    cons_result = word_beginning_with_n_consonants(word, 10)
+    consonents, how_many = cons_result[0], cons_result[1]
+    outp << word[how_many..inp.length] + consonents + "ay"
+  end
+  outp.join(" ")
+end
+
+def word_beginning_with_n_consonants(word, n)
   vowels = "aeiouAEIOU"
-  how_many_consonants_at_the_beginning.to_i.times do |i|
-    if vowels.include?(inp[i])
-      "#{inp}ay"
+  consonants_so_far, num_consonants = Array.new, 0
+  word.split("").each_with_index do |letter, index|
+    if vowels.include?(letter)
+      break
     else
-      "#{inp.reverse}ay"
+      consonants_so_far << letter
+      num_consonants = (index + 1)
     end
   end
+  [consonants_so_far.join(""), num_consonants]
 end
