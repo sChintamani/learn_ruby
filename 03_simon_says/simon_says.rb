73b4d1ea8fa51426ffbf49cc3e8c0f64e093907c
Srinivas Chintamani <srinivas.chintamani@gmail.com>
2018-09-01 00:18:46 +0530

    implemented start_of_word, first_word

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -11,3 +11,11 @@ def repeat(inp, how_many_times = 2)
   (how_many_times.to_i - 1).times { rpt += " #{inp}" }
   rpt
 end
+
+def start_of_word(inp, how_many_letters = 1)
+  inp.to_s[0..how_many_letters - 1]
+end
+
+def first_word(inp)
+  inp.split(" ")[0]
+end
