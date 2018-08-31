10a010d64f000a5cf4ccdf062f8286d7224f9368
Srinivas Chintamani <srinivas.chintamani@gmail.com>
2018-09-01 00:15:10 +0530

    implemented echo, shout and repeat

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,13 @@
-#write your code here
+def echo(inp)
+  inp
+end
+
+def shout(inp)
+  inp.upcase
+end
+
+def repeat(inp, how_many_times = 2)
+  rpt = inp
+  (how_many_times.to_i - 1).times { rpt += " #{inp}" }
+  rpt
+end
