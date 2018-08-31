c73001736ab800c296a1b1256a867b9978026868
Srinivas Chintamani <srinivas.chintamani@gmail.com>
2018-09-01 00:51:31 +0530

    started implementing pig latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,10 @@
-#write your code here
+def translate(inp, how_many_consonants_at_the_beginning = 1)
+  vowels = "aeiouAEIOU"
+  how_many_consonants_at_the_beginning.to_i.times do |i|
+    if vowels.include?(inp[i])
+      "#{inp}ay"
+    else
+      "#{inp.reverse}ay"
+    end
+  end
+end
