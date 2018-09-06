56c880de4a94c157f4ee5a937e289edc7efc23fc
Srinivas Chintamani <srinivas.chintamani@gmail.com>
2018-09-06 06:14:14 +0530

    implemented book title specs - re-using logic from simon-says unit.

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,25 @@
 class Book
-# write your code here
+  def title
+    @title
+  end
+
+  def title=(theTitle)
+    @title = capitalizeWords(theTitle)
+  end
+
+  private
+
+  def capitalizeWords(inp)
+    little_words = %w(a an the and in of)
+    inp_arr = inp.split(" ")
+    out_arr = []
+    inp_arr.each_with_index do |word, index|
+      if little_words.include?(word) && index != 0
+        out_arr << word
+      else
+        out_arr << word[0].upcase + word[1..word.length]
+      end
+    end
+    out_arr.join(" ")
+  end
 end
